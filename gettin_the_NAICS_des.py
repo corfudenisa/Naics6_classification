232252a04b8e20a091fbf4adbbfde576ea1b24c8
Paun Darius <paundariusalexandandru@gmail.com>
2024-04-20 17:32:25 +0300

    data done

--- gettin_the_NAICS_des.py
+++ gettin_the_NAICS_des.py
@@ -1,35 +1,31 @@
 import pandas as pd
+# dictData = {}
+# prefix = [32, 42, 54]
+# df = pd.read_excel("../2022-NAICS-Keywords.xlsx")
 
-dictData = {}
-dictData24 ={}
-prefix = [32, 42, 54]
-df = pd.read_excel("../2022-NAICS-Keywords.xlsx")
-
-for index, row in df.iloc[:10000].iterrows():
-    values = row.values  # Get the values of the row
-    val = int(values[0])  # Convert values[0] to integer
-    if val // 10000 in prefix:
-        key = f"{values[0]}"
-        key2 = f"{values[0]/10000}"
-        if key not in dictData24: 
-            description = {}
-            subcategories = {}
-            dictData[key] = {}
-        dictData24[]
-        if key not in dictData:
-            dictData[key] = []
-        dictData[key].append(values[2])
+# for index, row in df.iterrows():
+#     values = row.values  # Get the values of the row
+#     try:
+#         val = int(values[0])  # Convert values[0] to integer
+#         if val // 10000 in prefix:
+#             key = f"{values[0]}"
+#             if key not in dictData:
+#                 dictData[key] = {"Name":values[1],"Description": []}
+                
+#             dictData[key]["Description"].append(values[2])
+#     except:
+#         pass
    
 
 
-import json
+# import json
 
 
 
 
-# Define the file path
-file_path = "naics6.json"
+# # Define the file path
+# file_path1 = "naics6.json"
 
-# Write dictionary to JSON file
-with open(file_path, "w") as json_file:
-    json.dump(dictData, json_file, indent=4)
+# # Write dictionary to JSON file
+# with open(file_path1, "w") as json_file1:
+#     json.dump(dictData, json_file1, indent=4)
